7c0f62a4d66bbc98d4cc578865ac98fbba2a8a2b
Andrei Bombin <abombin93@gmail.com>
2022-05-07 19:50:47 -0400

    added data column selection for samples frequency

--- app.py
+++ app.py
@@ -1,21 +1,38 @@
 from dash import Dash, html, dcc
 import plotly.express as px
 import pandas as pd
-
 app = Dash(__name__)
 
 df=pd.read_csv("./data/shinyMetadat.csv")
-byState=df.groupby('State').size()
-byStateDf=byState.reset_index()
-mapping = {byStateDf.columns[0]: 'State', byStateDf.columns[1]: 'Freq'}
-byStateDfN=byStateDf.rename(columns=mapping)
+print(list(df.columns))
 
-fig=px.bar(byStateDfN, x="State", y="Freq")
+app.layout= html.Div([
+    html.H1('Viz app'),
+    html.Div([
+        dcc.Dropdown(id='dropdown',
+        options= [ 
+            {'label':'State', 'value':'State'},
+            {'label':'Streptomycin', 'value':'Streptomycin'},
+            ],
+        value='State')]),
+    dcc.Graph(id='Freq')
 
-app.layout= html.Div(children=[
-    html.H1(children='Viz app'),
-    dcc.Graph(id='Freq', figure=fig)
-])
+    ])
+
+from dash import Dash, dcc, html, Input, Output 
+
+@app.callback(
+    Output(component_id='Freq',component_property='figure'),
+    Input(component_id='dropdown', component_property='value')
+)
+def update_output_div(dropdown_value):
+    byGroup=df.groupby('{}'.format(dropdown_value)).size()
+    byGroupDf=byGroup.reset_index()
+    mapping = {byGroupDf.columns[0]: 'State', byGroupDf.columns[1]: 'Freq'}
+    byStateDfN=byGroupDf.rename(columns=mapping)
+    fig=px.bar(byStateDfN, x="State", y="Freq")
+    return fig
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
