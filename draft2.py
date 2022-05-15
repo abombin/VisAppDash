e8f5f47e131e61eff78f68c2d3e200ba54ff7b57
Andrei Bombin <abombin93@gmail.com>
2022-05-15 17:56:25 -0400

    Delete draft2.py

--- draft2.py
+++
@@ -1,9 +0,0 @@
-from dash import Dash, html, dcc
-import plotly.express as px
-import pandas as pd
-app = Dash(__name__)
-
-df=pd.read_csv("./data/shinyMetadat.csv")
-print(df.columns)
-val=df.columns[0]
-print(val)
